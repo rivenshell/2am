172e09e8d7f1ab4a378779fd161e8a40dd741520
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-05-08 02:41:25 -0400

    this entire site was built on 2am intervals

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("i love the abyss")
+
+import * as THREE from "three"
+
+//canvas
+const canvas = document.querySelector("canvas.webgl")
+const scene = new THREE.Scene()
+
+//object
+
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+//cam
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+
+//ren
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
